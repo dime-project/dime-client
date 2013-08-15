7e2a9c5f7e3c85e50b92fae01d00cf42a5a26ec9
patrick9789 <patricksvengrau@gmail.com>
2013-08-15 17:32:11 +0200

    FEATURE/FIX merged update, web ui issues (75daf, 099f2)

--- dime-client-model/resources/www/dime-communications/static/ui/dime/script/index.js
+++ dime-client-model/resources/www/dime-communications/static/ui/dime/script/index.js
@@ -363,7 +363,7 @@ DimeView = {
         jChildItem.append(DimeView.createMark(entry, "", false));
         
         //innerChild - name
-        var entryName = DimeView.getShortName(entry.name);
+        var entryName = DimeView.getShortNameWithLength(entry.name, 30);
         
         
         if (entry.type!==Dime.psMap.TYPE.USERNOTIFICATION){
@@ -420,7 +420,9 @@ DimeView = {
         if (selectingGroupName){
             containerCaption += ' in "' +selectingGroupName+'"';
         }else if (DimeView.searchFilter.length===0){
-            containerCaption = "All "+containerCaption;
+            if(jContainer.selector !== "#groupNavigation"){
+                containerCaption = "All "+containerCaption;
+            }
         }
 
         jContainer.empty();
@@ -702,6 +704,7 @@ DimeView = {
 
         var informationViewContainer = DimeView.getMetaListContainer(DimeView.CONTAINER_ID_INFORMATION);  
         
+        console.log(entry);
         //informationViewContainer.append(DimeView.createMetaBarListItem(entry.name,"", entry.imageUrl));
         informationViewContainer.append(DimeView.createMetaBarListItem(DimeView.getShortNameWithLength(entry.name, 35), "", entry.imageUrl));
         informationViewContainer.append(DimeView.createMetaBarListItem(
@@ -712,7 +715,7 @@ DimeView = {
             var setProviderName=function(response){
                 if (response){
                 informationViewContainer.append(DimeView.createMetaBarListItem(
-                    "provided by:", response.name, response.imageUrl));
+                    "shared by:", response.name, response.imageUrl));
                 }
             };
             Dime.REST.getItem(entry.userId, Dime.psMap.TYPE.PERSON, setProviderName, '@me', this);
@@ -1812,6 +1815,7 @@ Dime.Settings = {
     },
 
     deactivateServiceAccount: function(event, element, serviceAccount) {
+        $("#lightBoxBlack").fadeOut(300);
         $('#ConfigServiceDialogID').remove();
         var callBackHandler = function(response) {
             console.log("DELETED service " + serviceAccount.guid + " - response:", response);
@@ -1908,7 +1912,7 @@ Dime.Settings = {
                                                 (new Dime.Dialog.Toast("Please try again! - Your passwords didn't match!")).showLong();
                                             }else{
                                                 user.password=myPass;
-                                                Dime.REST.updateUser(user,Dime.Settings.updateSettings, Dime.Settings);
+                                                Dime.REST.updateUser(user, Dime.Settings.updateSettings, Dime.Settings);
                                                 (new Dime.Dialog.Toast("Password updated successfully!")).show();
                                             }
                                             myPass=null;
